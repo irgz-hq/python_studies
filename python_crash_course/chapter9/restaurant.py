83f308a1040f408da5bc2a4d6c37377b80293e39
irgzada <luiguiaugusto.ls@gmail.com>
2023-11-19 20:39:19 -0300

    commitchapter9

---
+++ python_crash_course/chapter9/restaurant.py
@@ -0,0 +1,18 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        prompt = f'The restaurant name is {self.restaurant_name} '
+        prompt += f'and the cuisine type is {self.cuisine_type}'
+        print(prompt)
+
+    def open_restaurant(self):
+        print("The restaurant is open")
+
+restaurant = Restaurant("Restaurante Universitário", "lunch")
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
